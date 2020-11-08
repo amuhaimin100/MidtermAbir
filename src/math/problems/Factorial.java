e6402d2e610613766dd79d51b7a8ed92525f3222
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-08 16:54:10 -0600

    math1&2

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -8,6 +8,30 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        //a: using Recursion
+        //b: using Iteration
+
+        int factorial = fct(5);                                     //a7. create container for the return value
+        System.out.println("a. Factorial of 5 is: " + factorial);      //a8. print factorial using Recursion
+
+        int number = 5;                                                  //b1. create variable number declare value of 5
+        int fact = 1;                                                    //b2. create variable fact and value 1
+        for (int i = 1; i <= number; i++) {                              //b3. create for loop
+            fact = fact * i;                                             //b4. everytime fact is called it will multiply the next number with 1 until it reaches 5
+        }
+        System.out.println("b. Factorial of 5 is: " + fact);             //b5. print factorial using iteration
 
     }
+
+    public static int fct(int f) {                                      //a1. create a static return method with a int parameter
+        int output;                                                     //a2. declare in variable output
+        if (f == 1) {                                                   //a3.add condition
+
+            return 1;                                                   //a4. return value
+        }
+        output = fct(f - 1) * f;                                     //a5.Recursion: calling itself
+        return output;                                                  //a6. return variable
+    }
+
 }
+
--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -8,5 +8,17 @@ public class Fibonacci {
           0,1,1,2,3,5,8,13
          */
 
+        int fib1= 40;                               //1. create variable with value of how many numbers we need to get
+        int fib2= 0;                                //2. number 1 of the sequence
+        int fib3= 1;                                //3. second number of the sequence
+
+        System.out.println("First "+fib1+ " numbers: ");
+
+        for(int i=1; i<=fib1; i++){                 //4. create for loop start at the first number of the sequence until it reaches 40
+            System.out.println(fib2);               //5. start by printing number 1 of the sequence 0
+            int sum= fib2+fib3;                     //6. loop should start at 0 and add 1
+            fib2=fib3;                              //7. on the next sequence the number should be the same as the previous number
+            fib3=sum;                               //8. then keep adding the previous 2 numbers
+        }
     }
 }
