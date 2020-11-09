a576327fbd2b81c270daa03b82e351f036d28471
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-08 18:50:34 -0600

    palindrome

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -8,5 +8,28 @@ public class Palindrome {
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
 
+        String str = "rotator";
+
+        if (isPalindrome(str))
+            System.out.print("Yes");
+        else
+            System.out.print("No");
+
     }
+
+    static boolean isPalindrome(String str) {
+
+        int i = 0, j = str.length() - 1;
+
+        while (i < j) {
+
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+
+            i++;
+            j--;
+        }
+        return true;
+    }
+
 }
