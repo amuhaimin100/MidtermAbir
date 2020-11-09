1326f1f2cc607710356bbe86513a65adb37fdc84
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-09 12:59:05 -0600

    queue

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,7 @@
 package datastructure;
-
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 public class UseQueue {
 
     public static void main(String[] args) {
@@ -9,6 +11,38 @@ public class UseQueue {
          *
          */
 
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < 5; i++)
+            q.add(i);                                           //add elements to queue
+
+
+        System.out.println("Elements of queue "                 //display elements of queue
+                + q);
+
+
+        int removedele = q.remove();                            // To remove the head of queue.
+        System.out.println("removed element-"
+                + removedele);
+
+        System.out.println(q);
+
+
+        int head = q.peek();                                    // To view the head of queue: use peek
+        System.out.println("head of queue-"
+                + head);
+
+
+        int size = q.size();
+        System.out.println("Size of queue-"
+                + size);
+
+
+        Iterator<Integer> itr = q.iterator();                   //use iterator and while loop
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
     }
+
 
 }
