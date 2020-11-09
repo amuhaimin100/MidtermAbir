1b00105e5a1e0b71ec7b5d86cc3dddd574a1c73f
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-09 12:27:02 -0600

    arraylist

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class UseArrayList {
 
     public static void main(String[] args) {
@@ -9,6 +12,24 @@ public class UseArrayList {
          * Store all the sorted data into one of the databases.
          *
          */
+
+        ArrayList<String> cities = new ArrayList<>();
+        cities.add("NYC");                                    //add data
+        cities.add("Las Vegas");
+        cities.add("Boston");
+        cities.add("Dallas");
+        cities.remove(1);                               //remove index:1 Las Vegas
+
+
+        System.out.println(cities.get(0));                    //retrieve element
+
+
+        Iterator iterator = cities.iterator();               // Use Iterator and while loop
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        cities.forEach(city -> System.out.println(city));    //use for each method
 
     }
 
