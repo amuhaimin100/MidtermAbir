e60ec71a7dc6921a97d666b15f19ccdafa136c52
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-11 11:06:34 -0600

    largestWord

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -14,6 +14,18 @@ public class DetermineLargestWord {
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
+        String[] word = s.split(" ");
+
+        String largestWord = "";
+        for (int i = 0; i < word.length; i++) {
+            if (word[i].length() > largestWord.length()) {
+                largestWord = word[i];
+
+            }
+        }
+
+        System.out.println(largestWord.length() + " " + largestWord);
+
     }
 
     public static Map<Integer, String> findTheLargestWord(String wordGiven) {
