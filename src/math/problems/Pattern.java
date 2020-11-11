c21c93ae5972f18b5c9d93efb3d0722116dbec54
Abir muhaimin <am@Abirs-MacBook-Pro.local>
2020-11-11 17:29:00 -0600

    updatepatter

--- src/math/problems/Pattern.java
+++ src/math/problems/Pattern.java
@@ -9,25 +9,25 @@ public class Pattern {
          *
          */
 
-        for(int i = 100; i >= 0; i--) {                     //start from 100 stop at 0
+        for (int i = 100; i >= 0; i--) {                     //start from 100 stop at 0
             System.out.println(i);
-            if(i == 90) {                                   //complete loop until reach 90
+            if (i == 90) {                                   //complete loop until reach 90
                 break;
             }
         }
-        for(int i = 88; i >= 0; i=i-2) {                    //from 88, the pattern changes to -2 increments
+        for (int i = 88; i >= 0; i = i - 2) {                    //from 88, the pattern changes to -2 decrements
             System.out.println(i);
-            if(i == 70) {                                   //complete loop until reach 70
+            if (i == 70) {                                   //complete loop until reach 70
                 break;
             }
         }
-        for(int i = 67; i >= 0; i=i-3) {                    //from 67 until 40 the increment is -3
+        for (int i = 67; i >= 0; i = i - 3) {                    //from 67 until 40 the decrement is -3
             System.out.println(i);
-            if(i == 40) {
+            if (i == 40) {
                 break;
             }
         }
-        for(int i = 36; i >= 0; i=i-4) {                    //from 40-0, increment of 4. Can also be broken down to larger increments
+        for (int i = 36; i >= 0; i = i - 4) {                    //from 36-0, decrement of -4.
             System.out.println(i);
         }
 
